4eaba0e6097243082efa8d1c5c07f0edc38a4838
Pawan Joshi <joshipawan2021@gmail.com>
2026-08-23 15:01:24 +0530

    fix: disable Tawk.to attention grabber and popup cards to keep live chat clean

--- src/components/TawkChat.tsx
+++ src/components/TawkChat.tsx
@@ -10,6 +10,14 @@ export default function TawkChat() {
       dangerouslySetInnerHTML={{
         __html: `
           var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+          Tawk_API.disableAttentionGrabber = true;
+          Tawk_API.onLoad = function(){
+            try {
+              if (typeof Tawk_API.hideAttentionGrabber === 'function') {
+                Tawk_API.hideAttentionGrabber();
+              }
+            } catch(e) {}
+          };
           (function(){
             var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
             s1.async = true;
